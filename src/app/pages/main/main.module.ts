7a47a007abe5297bc55ebc03e7bf7bdd1b8dd788
Kim Do <du.qixinkim@gmail.com>
2022-05-11 23:37:39 -0400

    Added scss

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -4,15 +4,10 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    MainComponent
-  ],
-  imports: [
-    CommonModule,
-    MainRoutingModule,
-    MatSliderModule
-  ]
+  declarations: [MainComponent],
+  imports: [CommonModule, MainRoutingModule, MatSliderModule, MatButtonModule],
 })
-export class MainModule { }
+export class MainModule {}
