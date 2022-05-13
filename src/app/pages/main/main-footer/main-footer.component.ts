02f2a9af89b09e28af7a262ebae2bddc8689e723
Kim Do <du.qixinkim@gmail.com>
2022-05-12 23:21:45 -0400

    Added packages

--- src/app/pages/main/main-footer/main-footer.component.ts
+++ src/app/pages/main/main-footer/main-footer.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-main-footer',
   templateUrl: './main-footer.component.html',
-  styleUrls: ['./main-footer.component.scss']
+  styleUrls: ['./main-footer.component.scss'],
 })
 export class MainFooterComponent implements OnInit {
-
-  constructor() { }
+  currentYear: string;
+  constructor() {}
 
   ngOnInit(): void {
+    this.currentYear = `${dayjs().year()}`;
   }
-
 }
