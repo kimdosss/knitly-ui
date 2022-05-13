2fcbca6be167bbbb22e5410e17a02db706afd3a8
Kim Do <du.qixinkim@gmail.com>
2022-05-12 22:43:23 -0400

    Added nav

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -6,9 +6,10 @@ import { MainComponent } from './main.component';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MainContentComponent } from './main-content/main-content.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
 
 @NgModule({
-  declarations: [MainComponent, MainContentComponent],
+  declarations: [MainComponent, MainContentComponent, MainNavComponent],
   imports: [CommonModule, MainRoutingModule, MatSliderModule, MatButtonModule],
 })
 export class MainModule {}
